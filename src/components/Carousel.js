3f71bb4b227e7a26ff28cde602318d6d64f46886
GPREC6 <116934235+LABBISRIKANTHBABU@users.noreply.github.com>
2025-12-13 15:59:52 +0530

    added carsoule images

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -5,27 +5,27 @@ import '../styles/Carousel.css';
 const INITIAL_ITEMS = [
   {
     id: 1,
-    src: 'C:\Users\kanth\OneDrive\Desktop\TechMatch\public\assets\car1.png',
+    image: '/assets/car1.png',
   },
   {
     id: 2,
-    image: 'https://drive.google.com/file/d/13TVaG6g5hbGDmMnOTuXIpAWy8JxeEERz/view?usp=sharing',
+    image: '/assets/car2.png',
   },
   {
     id: 3,
-    image: 'https://drive.google.com/file/d/1uycY8nNI7ptjw6vFe5nIVTg2Ipysl_Z6/view?usp=sharing',
+    image: '/assets/car3.png',
   },
   {
     id: 4,
-    image: 'https://drive.google.com/file/d/1ZGG2l_JwydK4Yb0uawxckbWoNhRbZqT5/view?usp=sharing',
+    image: '/assets/car4.png',
   },
   {
     id: 5,
-    image: 'https://drive.google.com/file/d/105kGU4XPhkq31JrWYPX-uNlktc7m3VhD/view?usp=sharing',
+    image: '/assets/car5.png',
   },
   {
     id: 6,
-    image: 'https://drive.google.com/file/d/1JWpIt3k8MCl0lKtPwAaCiJLF43eN1Var/view?usp=sharing',
+    image: '/assets/car6.png',
   },
 ];
 
